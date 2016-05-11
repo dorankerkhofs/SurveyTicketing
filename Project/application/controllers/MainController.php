37ab4393a0c93cef79671e2d31affa649707d8fe
Doran <doran_kerkhofs@hotmail.com>
2016-05-11 13:47:07 +0200

    no message

--- Project/application/controllers/MainController.php
+++ Project/application/controllers/MainController.php
@@ -9,3 +9,4 @@ class MainController extends CI_Controller
 
     }
 }
+
--- Project/application/views/MainView.php
+++ Project/application/views/MainView.php
@@ -1,2 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>MainView</title>
+
+</head>
+
+<body>
+</body>
+
+</html>
+
+
 <?php
-echo "het werkt";
